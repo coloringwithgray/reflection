70325984d153ed21eb77f5996e5927b7a665f4f5
coloringwithgray <kayron@houseofcalloway.com>
2024-06-14 20:16:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const text = "Reflections of (You)";
     let index = 0;
     const speed = 150; // Typing speed in milliseconds
+    const delay = 1370; // Delay before starting the animation in milliseconds
     const textContainer = document.getElementById("animated-text");
 
     function type() {
         if (index < text.length) {
-            textContainer.innerHTML += text.charAt(index);
+            textContainer.innerHTML += text.charAt(index) + "<br>";
             index++;
             setTimeout(type, speed);
         }
     }
 
-    type();
+    setTimeout(type, delay);
 });
