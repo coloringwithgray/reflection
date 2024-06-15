0d8b79099ccedee811cb811c5e7f60f5dcdd8140
coloringwithgray <kayron@houseofcalloway.com>
2024-06-14 20:32:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const text = "Reflections of (You)";
     let index = 0;
-    const speed = 150; // Typing speed in milliseconds
-    const delay = 1370; // Delay before starting the animation in milliseconds
+    const speed = 250; // Typing speed in milliseconds (slowed down)
+    const delay = 3000; // Delay before starting the animation in milliseconds
     const textContainer = document.getElementById("animated-text");
 
     function type() {
