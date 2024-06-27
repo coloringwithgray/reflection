991b17a46c29057bb23dda60bef86fca89b2c776
coloringwithgray <kayron@houseofcalloway.com>
2024-06-27 11:46:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function() {
     const text = "Reflections of (You)";
     let index = 0;
-    const speed = 250;
-    const delay = 3000;
+    const speed = 250; // Typing speed in milliseconds (slowed down)
+    const delay = 3000; // Delay before starting the animation in milliseconds
     const textContainer = document.getElementById("animated-text");
 
     function type() {
@@ -15,10 +16,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setTimeout(type, delay);
 
+    // Check if the page is in an iframe or a portal
     if (window.self !== window.top) {
         document.body.classList.add('is-portal');
     }
 
+    // Handle scroll prompt visibility
     const scrollPrompt = document.querySelector('.scroll-prompt');
     window.addEventListener('scroll', function() {
         const scrollPosition = window.scrollY || document.documentElement.scrollTop;
